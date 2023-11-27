ac61105a5b5fcddc6a393fbaa2b3847101207f52
Pawank06 <pawan06kumar2003@gmail.com>
2023-11-27 18:08:13 +0530

    added frontend

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
+
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
