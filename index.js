36edfcc07763f8045e0a4b5efe7b9e2374cb457f
Pawank06 <pawan06kumar2003@gmail.com>
2023-11-30 18:02:03 +0530

    uibuild

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ app.put("/api/notes/:id", (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
